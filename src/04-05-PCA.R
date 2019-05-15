5761124bf0004808ad05a4d49220eebdc55c0455
garlandxie <garland.xie@gmail.com>
2019-05-15 16:01:27 -0300

    Initial commit

---
+++ src/04-05-PCA.R
@@ -0,0 +1,36 @@
+# libraries --------------------------------------------------------------------
+library(here)
+library(dplyr)
+library(tidyr)
+
+# import -----------------------------------------------------------------------
+traits_EF_clean_df <- readRDS(here::here("data/project_data/final",
+                                         "traits_EF_clean_df.rds"))
+
+# pca --------------------------------------------------------------------------
+
+# WD
+pca_matrix_wd <- traits_EF_clean_df %>%
+  ungroup() %>%
+  filter(treatment == "WD") %>%
+  select(rmf, srl, rld, max_root_depth_cm, mean_radius_mm, plant_size) %>%
+  drop_na %>% # complete cases
+  prcomp(center = TRUE, scale = TRUE)
+
+summary(pca_matrix_wd) # info on cumulative proportion
+
+# WW
+pca_matrix_ww <- traits_EF_clean_df %>%
+  ungroup() %>%
+  filter(treatment == "WW") %>%
+  select(rmf, srl, rld, max_root_depth_cm, mean_radius_mm, plant_size) %>%
+  drop_na %>% # complete cases
+  prcomp(center = TRUE, scale = TRUE)
+
+summary(pca_matrix_ww)  # info on cumulative proportion 
+
+# save the data ----------------------------------------------------------------
+pca_matrix_WD$x$PC1
+saveRDS(pca_matrix_wd, here("data/project_data/final", "pca_matrix_WD.rds"))
+saveRDS(pca_matrix_ww, here("data/project_data/final", "pca_matrix_WW.rds"))
+
